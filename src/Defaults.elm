8cf8df1142f6515d64558af1f0403d0fdefd6898
Marc Walter <walter.marc@outlook.com>
2024-05-19 20:21:45 +0200

    Working encoding
    TODO add prefix characters to fill min length
    
    TODO handle blocked words

--- src/Defaults.elm
+++ src/Defaults.elm
@@ -1,6 +1,36 @@
-module Defaults exposing (alphabet)
+module Defaults exposing
+    ( Context
+    , abc
+    , alphabet
+    , context
+    )
+
+import Array exposing (Array)
+
+
+type alias Context =
+    { alphabet : Array Char, minLength : Int, blockList : List String }
 
 
 alphabet : String
 alphabet =
     "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
+
+
+context : Context
+context =
+    -- shuffled default alphabet
+    { alphabet = "fwjBhEY2uczNPDiloxmvISCrytaJO4d71T0W3qnMZbXVHg6eR8sAQ5KkpLUGF9" |> String.toList |> Array.fromList
+    , minLength = 0
+    , blockList = []
+    }
+
+
+{-| TODO remove this example context after first tests
+-}
+abc : Context
+abc =
+    { alphabet = "abc" |> String.toList |> Array.fromList
+    , minLength = 0
+    , blockList = []
+    }
