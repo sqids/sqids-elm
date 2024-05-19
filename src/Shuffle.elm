0ad15fe5300167bce0d4fde21e5410eab00266a5
Marc Walter <walter.marc@outlook.com>
2024-05-19 17:01:53 +0200

    Extract Shuffle module

---
+++ src/Shuffle.elm
@@ -0,0 +1,83 @@
+module Shuffle exposing
+    ( charArray
+    , string
+    )
+
+import Array exposing (Array)
+
+
+{-| consistent shuffle (always produces the same result given the input)
+From <https://github.com/sqids/sqids-spec/blob/40f407169fa0f555b93a197ff0a9e974efa9fba6/src/index.ts#L244>
+
+    function shuffle(alphabet) {
+        const chars = alphabet.split('');
+
+        for (let i = 0, j = chars.length - 1; j > 0; i++, j--) {
+            const r = (i * j + chars[i].codePointAt(0) + chars[j].codePointAt(0)) % chars.length;
+            [chars[i], chars[r]] = [chars[r], chars[i]];
+        }
+
+        return chars.join('');
+    }
+
+-}
+string : String -> Result ShuffleError String
+string input =
+    let
+        length =
+            String.length input
+
+        chars =
+            input |> String.toList |> Array.fromList
+    in
+    shuf 0 (length - 1) length chars
+        |> Result.map (Array.toList >> String.fromList)
+
+
+type ShuffleError
+    = CharDoesNotExist Int (Array Char)
+
+
+charArray : Array Char -> Result ShuffleError (Array Char)
+charArray input =
+    let
+        length =
+            Array.length input
+    in
+    shuf 0 (length - 1) length input
+
+
+shuf : Int -> Int -> Int -> Array Char -> Result ShuffleError (Array Char)
+shuf i j length chars =
+    case
+        ( Array.get i chars |> Maybe.map Char.toCode
+        , Array.get j chars |> Maybe.map Char.toCode
+        )
+    of
+        ( Just iCharCode, Just jCharCode ) ->
+            let
+                r =
+                    (i * j + iCharCode + jCharCode)
+                        |> modBy length
+            in
+            case Array.get r chars of
+                Just rChar ->
+                    let
+                        arr =
+                            Array.set i rChar chars
+                                |> Array.set r (Char.fromCode iCharCode)
+                    in
+                    if j > 1 then
+                        shuf (i + 1) (j - 1) length arr
+
+                    else
+                        Ok arr
+
+                Nothing ->
+                    CharDoesNotExist r chars |> Err
+
+        ( Nothing, _ ) ->
+            CharDoesNotExist i chars |> Err
+
+        ( _, Nothing ) ->
+            CharDoesNotExist j chars |> Err
--- tests/Internal/Shuffle.elm
+++ tests/Internal/Shuffle.elm
@@ -1,6 +1,6 @@
 module Internal.Shuffle exposing (..)
 
-import Array exposing (Array)
+import Shuffle
 import Expect
 import Test exposing (Test)
 
@@ -11,20 +11,6 @@ defaultOptions =
     }
 
 
-{-| From <https://github.com/sqids/sqids-spec/blob/40f407169fa0f555b93a197ff0a9e974efa9fba6/tests/internal/shuffle.test.ts>
-
-const shuffle = (alphabet: string): string => {
-const chars = alphabet.split('');
-
-for (let i = 0, j = chars.length - 1; j > 0; i++, j--) {
-const r = (i \* j + chars[i].codePointAt(0) + chars[j].codePointAt(0)) % chars.length;
-[chars[i], chars[r]] = [chars[r], chars[i]];
-}
-
-return chars.join('');
-};
-
--}
 shuffleTest =
     [ test "default shuffle, checking for randomness"
         defaultOptions.alphabet
@@ -64,82 +50,5 @@ test : String -> String -> String -> Test
 test title input output =
     Test.test title <|
         \() ->
-            shuffle input
+            Shuffle.string input
                 |> Expect.equal (Ok output)
-
-
-{-| consistent shuffle (always produces the same result given the input)
-From <https://github.com/sqids/sqids-spec/blob/40f407169fa0f555b93a197ff0a9e974efa9fba6/src/index.ts#L244>
-
-    function shuffle(alphabet) {
-        const chars = alphabet.split('');
-
-        for (let i = 0, j = chars.length - 1; j > 0; i++, j--) {
-            const r = (i * j + chars[i].codePointAt(0) + chars[j].codePointAt(0)) % chars.length;
-            [chars[i], chars[r]] = [chars[r], chars[i]];
-        }
-
-        return chars.join('');
-    }
-
--}
-shuffle : String -> Result ShuffleError String
-shuffle input =
-    let
-        length =
-            String.length input
-
-        chars =
-            input |> String.toList |> Array.fromList
-    in
-    shuf 0 (length - 1) length chars
-        |> Result.map (Array.toList >> String.fromList)
-
-
-type ShuffleError
-    = CharDoesNotExist Int (Array Char)
-
-
-charArray : Array Char -> Result ShuffleError (Array Char)
-charArray input =
-    let
-        length =
-            Array.length input
-    in
-    shuf 0 (length - 1) length input
-
-
-shuf : Int -> Int -> Int -> Array Char -> Result ShuffleError (Array Char)
-shuf i j length chars =
-    case
-        ( Array.get i chars |> Maybe.map Char.toCode
-        , Array.get j chars |> Maybe.map Char.toCode
-        )
-    of
-        ( Just iCharCode, Just jCharCode ) ->
-            let
-                r =
-                    (i * j + iCharCode + jCharCode)
-                        |> modBy length
-            in
-            case Array.get r chars of
-                Just rChar ->
-                    let
-                        arr =
-                            Array.set i rChar chars
-                                |> Array.set r (Char.fromCode iCharCode)
-                    in
-                    if j > 1 then
-                        shuf (i + 1) (j - 1) length arr
-
-                    else
-                        Ok arr
-
-                Nothing ->
-                    CharDoesNotExist r chars |> Err
-
-        ( Nothing, _ ) ->
-            CharDoesNotExist i chars |> Err
-
-        ( _, Nothing ) ->
-            CharDoesNotExist j chars |> Err
